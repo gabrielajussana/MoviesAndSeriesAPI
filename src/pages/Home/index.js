d2f61fbbe182cfb975f54f233364757352cdfbc9
Gabriela <69367338+gabrielajussana@users.noreply.github.com>
2022-07-20 21:56:25 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,35 @@
+import {Container, MovieList, Movie} from "./styles";
+import {APIKey} from '../../config/key';
+import {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+
+function Home(){
+
+  const[movies, setMovies] = useState([])
+  const image_path = 'https://image.tmdb.org/t/p/w500'
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(data => setMovies(data.results))
+  }, [])
+
+  return (
+    <Container>
+    <h1>Movies</h1>
+    <MovieList>
+      {movies.map(movie => {
+        return(
+          <Movie key={movie.id}>
+            <Link to={`/details/${movie.id}`}><img src={`${image_path}${movie.poster_path}`} alt={movie.title}></img></Link>
+            <span>{movie.title}</span>
+          </Movie>
+        )
+      })}
+    </MovieList>
+    </Container>
+  )
+
+}
+
+export default Home;
