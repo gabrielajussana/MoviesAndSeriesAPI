dda99eb79236689fe8a4bbee1161b67aa651ecb2
Gabriela <69367338+gabrielajussana@users.noreply.github.com>
2022-07-21 20:50:46 -0300

    update Readme

--- Readme.md
+++ Readme.md
@@ -1,9 +1,10 @@
-#  Filmes e Séries Populares - API The Movie DB
+# 🎥  Filmes e Séries Populares - API The Movie DB
 ![GitHub stars](https://img.shields.io/github/stars/gabrielajussana/MoviesAndSeriesAPI?style=social)
+<br>
 Projeto React para consumir listas de filmes e séries populares do [The MovieDB API](https://developers.themoviedb.org/3/getting-started/introduction).
 Você pode ver detalhes como titulo, sinopse e data de lançamento ao clicar em uma opção de filme ou série.
 
-## Tecnologias
+## 💻 Tecnologias
 * HTML
 * CSS
 * JavaScript
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './global.css';
 import Home from './pages/Home';
 import Details from './pages/Details';
-
+import Series from './pages/Series';
+import SeriesDetails from './pages/SeriesDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/details/:id" element={<Details/>}/>
+    <Route path="/" element={<Home />}/>
+    <Route path="/details/:id" element={<Details />}/>
+    <Route path="/series" element={<Series />}></Route>
+    <Route path="/seriesdetails/:id" element={<SeriesDetails />}></Route>
   </Routes>
   </BrowserRouter>
 );
